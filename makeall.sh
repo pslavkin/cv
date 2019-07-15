84a0c2558061ff46b2118ea4c965df1412f6432c
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2019-07-15 02:46:28 -0300

    automatice la creaacion de combinacinoes es/es nrc/valencia
    buenisimo!! agregue un makeall.sh que automatiza la creacion de las
    diferentes combinaciones de lenguaje y bicacion, pero ya me queda para
    agregar mas cosas, este super cool, y ya te deja el resultado en su
    lugar con el nombre correspondiente.
    Utilice un truco que le pasas a pdflatex un \def y el archivo se la cree
    y actua en concecuencia.. no se como hacer con latexmk, pero ya veremos
    que invento.. talvez lo harcodeo mientras edito y luego lo vuelo...o
    algo asi      porque no se si latexmk le puedo pasar parametros como al
    pdflatex.. ya vere

---
+++ makeall.sh
@@ -0,0 +1,28 @@
+#espaniol
+pdflatex "\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/es/cv_es_portfolio_si_brc.pdf
+
+pdflatex "\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/es/cv_es_portfolio_no_brc.pdf
+
+pdflatex "\def\idiomaEs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/es/cv_es_portfolio_si_valencia.pdf
+
+pdflatex "\def\idiomaEs{}\def\portfolioNo{}\def\direccionValencia{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/es/cv_es_portfolio_no_valencia.pdf
+
+
+#ingles
+pdflatex "\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/us/cv_us_portfolio_si_brc.pdf
+
+pdflatex "\def\idiomaUs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/us/cv_us_portfolio_no_brc.pdf
+
+pdflatex "\def\idiomaUs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/us/cv_us_portfolio_si_valencia.pdf
+
+pdflatex "\def\idiomaUs{}\def\portfolioNo{}\def\direccionValencia{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/us/cv_us_portfolio_no_valencia.pdf
+
+
