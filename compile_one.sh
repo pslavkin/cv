439124c6cfa559ab8ba4b045a29e9c65c30821cd
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2019-11-10 19:38:48 -0300

    converti bastatnte a us y mejore mas la versatilidad
    avanzo con la idea del diccionario y agregue varias mejoras para hacer
    mas facil el mantenimiento. Agregue colores a algunas cosas y ordene un
    poco las tecnologias.. falta convertir algunos .tex de portfolio y
    agregar mas portfolio de otros proyectos que no estan aca aun.. tengo
    mil cosas por agregar...

--- compile_one.sh
+++ compile_one.sh
@@ -1,4 +1,4 @@
-#matexmk -pvc -pdf -pdflatex='pdflatex "\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
+latexmk -pvc -pdf -pdflatex='pdflatex "\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
 #latexmk -pvc -pdf -pdflatex='pdflatex "\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
-latexmk -pvc -pdf -pdflatex='pdflatex "\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
+#latexmk -pvc -pdf -pdflatex='pdflatex "\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
 #latexmk -pvc -pdf -pdflatex='pdflatex "\def\idiomaUs{}\def\portfolioNi{}\def\direccionBrc{}\input{cv}"'
