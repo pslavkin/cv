abe62b14e6661284cebf65af4f640cf39f3be812
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2019-11-10 23:38:18 -0300

    acomodo las pic en alta(500k) baja(50k) y original
    hice lo que tenia que hacer hace muuuucho tiempo.. deje 3 dirs,
    porrfolio alta, original y baja.. la baja a 50 por foto para que dentre
    en Linkedin, la alta a 500k para que se luzca y la original con lo que
    sea que tenga.. y cambien entonces las compilaciones para tener todas
    las variantes.. quedo bueno y tambien acorte los nombres para que no
    quede un link tan largo

--- makeall.sh
+++ makeall.sh
@@ -1,36 +1,57 @@
+#---------------------------------------------------
 #espaniol
-pdflatex "\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
-pdflatex "\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
-cp -v cv.pdf ./cv_pdf/es/cv_es_portfolio_si_brc.pdf
+#---------------------------------------------------
+pdflatex "\def\picHigh{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
+pdflatex "\def\picHigh{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/es/cv_es_high_brc.pdf
 
-pdflatex "\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"
-pdflatex "\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"
-cp -v cv.pdf ./cv_pdf/es/cv_es_portfolio_no_brc.pdf
+pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"
+pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/es/cv_es_brc.pdf
 
-pdflatex "\def\idiomaEs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
-pdflatex "\def\idiomaEs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
-cp -v cv.pdf ./cv_pdf/es/cv_es_portfolio_si_valencia.pdf
+pdflatex "\def\picHigh{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
+pdflatex "\def\picHigh{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/es/cv_es_high_valencia.pdf
 
-pdflatex "\def\idiomaEs{}\def\portfolioNo{}\def\direccionValencia{}\input{cv}"
-pdflatex "\def\idiomaEs{}\def\portfolioNo{}\def\direccionValencia{}\input{cv}"
-cp -v cv.pdf ./cv_pdf/es/cv_es_portfolio_no_valencia.pdf
+pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioNo{}\def\direccionValencia{}\input{cv}"
+pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioNo{}\def\direccionValencia{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/es/cv_es_valencia.pdf
 
+#low portfolio
+pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
+pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/es/cv_es_low_brc.pdf
 
+pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
+pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/es/cv_es_low_valencia.pdf
+
+#---------------------------------------------------
 #ingles
-pdflatex "\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
-pdflatex "\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
-cp -v cv.pdf ./cv_pdf/us/cv_us_portfolio_si_brc.pdf
+#---------------------------------------------------
+pdflatex "\def\picHigh{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
+pdflatex "\def\picHigh{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/us/cv_us_high_brc.pdf
 
-pdflatex "\def\idiomaUs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"
-pdflatex "\def\idiomaUs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"
-cp -v cv.pdf ./cv_pdf/us/cv_us_portfolio_no_brc.pdf
+pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"
+pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/us/cv_us_brc.pdf
 
-pdflatex "\def\idiomaUs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
-pdflatex "\def\idiomaUs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
-cp -v cv.pdf ./cv_pdf/us/cv_us_portfolio_si_valencia.pdf
+pdflatex "\def\picHigh{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
+pdflatex "\def\picHigh{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/us/cv_us_high_valencia.pdf
 
-pdflatex "\def\idiomaUs{}\def\portfolioNo{}\def\direccionValencia{}\input{cv}"
-pdflatex "\def\idiomaUs{}\def\portfolioNo{}\def\direccionValencia{}\input{cv}"
-cp -v cv.pdf ./cv_pdf/us/cv_us_portfolio_no_valencia.pdf
+pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioNo{}\def\direccionValencia{}\input{cv}"
+pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioNo{}\def\direccionValencia{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/us/cv_us_valencia.pdf
+
+#low portfolio
+pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
+pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/us/cv_us_low_brc.pdf
+
+pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
+pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"
+cp -v cv.pdf ./cv_pdf/us/cv_us_low_valencia.pdf
 
 
