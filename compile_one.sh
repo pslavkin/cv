54acd0998e64f4b07f855a9781f70353ac5a5a4c
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2019-11-18 04:03:01 -0300

    agrego mse_4_daso con asciinema es y us quedo bueno.
    faltaria pasar un par de cosas en el de us y meter las cosas de la cese
    y mse todo junto

--- compile_one.sh
+++ compile_one.sh
@@ -1,4 +1,4 @@
-latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
-#latexmk -pvc -pdf -pdflatex='pdflatex "\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
-#latexmk -pvc -pdf -pdflatex='pdflatex "\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
-#latexmk -pvc -pdf -pdflatex='pdflatex "\def\idiomaUs{}\def\portfolioNi{}\def\direccionBrc{}\input{cv}"'
+#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
+#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
+latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
+#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioNi{}\def\direccionBrc{}\input{cv}"'
