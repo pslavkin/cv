83d2aa090f77e4fe614f6576c067b8c93744e0a0
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2020-07-08 19:15:51 -0300

    agrego engineered arts y un link en la presentacion al propio cv en github

--- compile_one.sh
+++ compile_one.sh
@@ -1,6 +1,10 @@
-#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
-#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
-#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
-#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picHigh{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
+#es
 latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
-#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioNi{}\def\direccionBrc{}\input{cv}"'
+#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
+#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picHigh{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
+#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picHigh{}\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
+##us
+#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
+#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
+#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picHigh{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
+#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picHigh{}\def\idiomaUs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
