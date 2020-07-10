3a809447b9eda0ed61fd81b2e4244407ac190aa7
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2020-07-10 19:48:21 -0300

    cambio links de drive por github, codility y mas traduccion
    los links de drive estaban todos rotos, desde que cambie a github... muy
    mal, pero bueno, ya quedo...

--- compile_one.sh
+++ compile_one.sh
@@ -1,10 +1,10 @@
 #es
-latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
+#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
 #latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
 #latexmk -pvc -pdf -pdflatex='pdflatex "\def\picHigh{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
 #latexmk -pvc -pdf -pdflatex='pdflatex "\def\picHigh{}\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
 ##us
 #latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
 #latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
-#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picHigh{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
+latexmk -pvc -pdf -pdflatex='pdflatex "\def\picHigh{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
 #latexmk -pvc -pdf -pdflatex='pdflatex "\def\picHigh{}\def\idiomaUs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
