684a23115ea94fb13e0066f3deb5cee43bfdeaad
pablo slavkin <slavkin.pablo@gmail.com>
2021-09-12 23:21:29 -0300

    add novo and improve pic sizes

--- pic/high/scale_hi_quality.sh
+++ pic/high/scale_hi_quality.sh
@@ -1,1 +1,5 @@
-scale_jpg.sh 500
+echo 'try using mogrigy and jpegoptim instead of convert'
+#mogrify -resize 1152x738! ofi_dci*
+#mogrify -format jpg novo* 
+#jpegoptim -S 50k novo3.jpg 
+#scale_jpg.sh 500
