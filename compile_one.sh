0bda752ac7e28a5a7b9235a6f9b73300851f8a5b
pablo slavkin <slavkin.pablo@gmail.com>
2021-11-12 19:21:40 +0100

    PAL robotics added plus pinar address

--- compile_one.sh
+++ compile_one.sh
@@ -4,7 +4,7 @@
 #latexmk -pvc -pdf -pdflatex='pdflatex "\def\picHigh{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
 #latexmk -pvc -pdf -pdflatex='pdflatex "\def\picHigh{}\def\idiomaEs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
 ##us
-#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
-latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
+latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioSi{}\def\direccionBrc{}\input{cv}"'
+#latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaUs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
 #latexmk -pvc -pdf -pdflatex='pdflatex "\def\picLow{}\def\idiomaEs{}\def\portfolioSi{}\def\direccionValencia{}\input{cv}"'
 #latexmk -pvc -pdf -pdflatex='pdflatex "\def\picHigh{}\def\idiomaUs{}\def\portfolioNo{}\def\direccionBrc{}\input{cv}"'
